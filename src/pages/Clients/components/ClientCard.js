c14a91b9853669f855c3ecfcf4649e70402f3be3
illyakurochkin <illya.kurochkin@gmail.com>
2019-06-15 22:49:04 +0300

    Lot's of fixes and improvements
    (cherry picked from commit a6b4c1f179eaba1e1e06936be5982b7daf96398c)

---
+++ src/pages/Clients/components/ClientCard.js
@@ -0,0 +1,45 @@
+import React, {Component} from 'react';
+import {Icon, Item} from 'semantic-ui-react';
+import PropTypes from 'prop-types';
+
+class ClientCard extends Component {
+  render() {
+    const {client, onClick} = this.props;
+    
+    return (
+      <Item onClick={onClick}>
+        <Item.Image onClick={onClick} size="small" src={client.photo}/>
+        <Item.Content>
+          <Item.Header><a onClick={onClick}>{client.name}</a></Item.Header>
+          {client.age && (
+            <Item.Extra>
+              <Icon color="blue" name="star outline"/> {client.age} years
+            </Item.Extra>
+          )}
+          {client.sportRang && (
+            <Item.Extra>
+              <Icon color="blue" name="trophy"/> {client.sportRang}
+            </Item.Extra>
+          )}
+          {client.phone && (
+            <Item.Extra>
+              <Icon color='blue' name='phone'/> {client.phone}
+            </Item.Extra>
+          )}
+          {client.email && (
+            <Item.Extra>
+              <Icon color="blue" name="mail"/> {client.email}
+            </Item.Extra>
+          )}
+        </Item.Content>
+      </Item>
+    );
+  }
+}
+
+ClientCard.propTypes = {
+  client: PropTypes.object.isRequired,
+  onClick: PropTypes.func.isRequired
+};
+
+export default ClientCard;
