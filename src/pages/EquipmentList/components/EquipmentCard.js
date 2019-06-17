7a81fac53eb13c141cdf1dde22c2abd1a28612d7
illyakurochkin <illya.kurochkin@gmail.com>
2019-06-18 00:30:00 +0300

    Fix some shit

--- src/pages/EquipmentList/components/EquipmentCard.js
+++ src/pages/EquipmentList/components/EquipmentCard.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import {Image} from 'semantic-ui-react/index';
+import {Image} from 'semantic-ui-react';
 
 const Container = styled.div`
   padding: 10px;
