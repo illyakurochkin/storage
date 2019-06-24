7278ed4b2febb578032d37c9dc6f8188a9369f6f
illyakurochkin <illya.kurochkin@gmail.com>
2019-06-24 12:53:36 +0300

    Auth

--- src/redux/actions/authActions.js
+++ src/redux/actions/authActions.js
@@ -3,16 +3,20 @@ import api from '../../utils/api';
 import sha256 from '../../utils/sha256';
 
 export const signIn = (login, password) => async dispatch => {
-  const hashedPassword = password; //sha256(password);
-  const response = await api.post('/login', {login, password: hashedPassword});
+  try {
+    const hashedPassword = password; //sha256(password);
+    const response = await api.post('/login', {login, password: hashedPassword});
   
-  localStorage.setItem('Authorization', response.data);
-  api.defaults.headers.common.Authorization = response.data;
+    localStorage.setItem('Authorization', response.data);
+    api.defaults.headers.common.Authorization = response.data;
   
-  dispatch({
-    type: SIGN_IN,
-    token: response.data
-  });
+    dispatch({
+      type: SIGN_IN,
+      token: response.data
+    });
+  } catch (e) {
+    alert('incorrect login or password');
+  }
 };
 
 export const signOut = () => {
